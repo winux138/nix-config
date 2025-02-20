2170b14242a922a965a98c7a1c43d0e49d4f5790
juthomas <juthomas@tandemdiabetes.com>
2025-02-20 14:14:32 +0100

    Fix TODOs and FIXMEs

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -45,10 +45,9 @@
     };
   };
 
-  # TODO: Set your username
   home = {
-    username = "your-username";
-    homeDirectory = "/home/your-username";
+    username = "ju";
+    homeDirectory = "/home/ju";
   };
 
   # Add stuff for your user as you see fit:
